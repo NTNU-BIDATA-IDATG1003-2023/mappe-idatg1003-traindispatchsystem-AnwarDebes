566325a91dbd55ea1c2b06ae0f0497592c690b8b
anward@stud.ntnu.no <anwardebs6@gmail.com>
2023-10-29 12:34:32 +0100

    writing JavaDoc for CheckValid and TrainDeparture classes

--- src/main/java/edu/ntnu/stud/CheckValid.java
+++ src/main/java/edu/ntnu/stud/CheckValid.java
@@ -3,38 +3,82 @@ package edu.ntnu.stud;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * The CheckValid class provides static methods to perform various validation checks.
+ */
 public  class CheckValid {
-
-
-
-
+	/**
+	 * Checks if the provided string is not null, empty, or blank.
+	 *
+	 * @param string            The string to be checked.
+	 * @param exceptionMessage  The message to be used in the exception if the check fails.
+	 * @return True if the string is valid.
+	 * @throws IllegalArgumentException If the string is null, empty, or blank.
+	 */
   public static Boolean checkValidString(String string, String exceptionMessage) {
 	if 	(string == null || string.isEmpty() || string.isBlank()){
 	  throw new IllegalArgumentException(exceptionMessage);
 	}
 	return true;
   }
+	/**
+	 * Checks if the provided integer is a positive number.
+	 *
+	 * @param number            The number to be checked.
+	 * @param exceptionMessage  The message to be used in the exception if the check fails.
+	 * @return True if the number is positive.
+	 * @throws IllegalArgumentException If the number is negative.
+	 */
   public static Boolean checkValidInt(int number, String exceptionMessage) {
 	if(number < 0) {
 	  throw new IllegalArgumentException(exceptionMessage);
 	}
 	return true;
-  } public static Boolean checkValidInt(int number) {
+  }
+	/**
+	 * Checks if the provided integer is a positive number.
+	 *
+	 * @param number  The number to be checked.
+	 * @return True if the number is positive.
+	 */
+  public static Boolean checkValidInt(int number) {
 	return (number> 0);
   }
+	/**
+	 * Checks if the provided track number is not less than -1.
+	 *
+	 * @param number            The track number to be checked.
+	 * @param exceptionMessage  The message to be used in the exception if the check fails.
+	 * @return True if the track number is valid.
+	 * @throws IllegalArgumentException If the track number is less than -1.
+	 */
   public static Boolean checkValidTrackNumber(int number, String exceptionMessage) {
 	if(number < -1) {
 	  throw new IllegalArgumentException(exceptionMessage);
 	}
 	return true;
   }
-
+	/**
+	 * Checks if the provided time is in the format hh:mm.
+	 *
+	 * @param time              The time to be checked.
+	 * @param exceptionMessage  The message to be used in the exception if the check fails.
+	 * @return True if the time is in the correct format.
+	 * @throws IllegalArgumentException If the time is not in the format hh:mm.
+	 */
   public static Boolean checkValidTime(String time, String exceptionMessage) {
 	if(!time.matches("(2[0-3]|[01][0-9]):[0-5][0-9]")){
 	  throw new IllegalArgumentException(exceptionMessage);
 	}
 	return 	true;
   }
+	/**
+	 * Checks if the provided map of TrainDepartures is not empty.
+	 *
+	 * @param map  The map to be checked.
+	 * @return True if the map is not empty.
+	 * @throws IllegalArgumentException If the map is empty or null.
+	 */
   public  static boolean checkMAp(Map<Integer,TrainDeparture> map) {
 
 	if (map.isEmpty() || map == null) {
@@ -42,5 +86,4 @@ public  class CheckValid {
 	}
 	return true;
   }
-
 }
--- src/main/java/edu/ntnu/stud/TrainDeparture.java
+++ src/main/java/edu/ntnu/stud/TrainDeparture.java
@@ -1,6 +1,9 @@
 package edu.ntnu.stud;
 
-
+/**
+ * The TrainDeparture class represents the departure details of a train. It includes information such as
+ * the departure time, line, train number, destination, track, and delay.
+ */
 public class TrainDeparture {
 
 
@@ -16,6 +19,16 @@ public class TrainDeparture {
 
   private String delay;
 
+	/**
+	 * Constructs a new TrainDeparture instance with the specified details.
+	 *
+	 * @param departureTime The time of departure in the format hh:mm.
+	 * @param line          The line on which the train operates.
+	 * @param trainNumber   The number assigned to the train.
+	 * @param destination   The destination of the train.
+	 * @param track         The track from which the train departs.
+	 * @param delay         The delay of the train in the format hh:mm.
+	 */
   public TrainDeparture(String departureTime, String line, int trainNumber, String destination, int track,
 						String delay) {
 	setDepartureTime(departureTime);
@@ -26,12 +39,21 @@ public class TrainDeparture {
 	setDelay(delay);
   }
 
-
+	/**
+	 * Returns the departure time of the train.
+	 *
+	 * @return The departure time in the format hh:mm.
+	 */
   public String getDepartureTime() {
 
 	return departureTime;
   }
 
+	/**
+	 * Sets the departure time of the train after validating the input.
+	 *
+	 * @param departureTime The departure time in the format hh:mm.
+	 */
   public void setDepartureTime(String departureTime) {
 
 	if (CheckValid.checkValidString(departureTime, "Time needs to be a string") &&
@@ -41,11 +63,21 @@ public class TrainDeparture {
 	}
   }
 
+	/**
+	 * Returns the line on which the train operates.
+	 *
+	 * @return The line of the train.
+	 */
   public String getLine() {
 
 	return line;
   }
 
+	/**
+	 * Sets the line of the train after validating the input.
+	 *
+	 * @param line The line on which the train operates. It must be a valid string.
+	 */
   public void setLine(String line) {
 
 	if (CheckValid.checkValidString(line, "Line needs to be a string")) {
@@ -53,11 +85,21 @@ public class TrainDeparture {
 	}
   }
 
+	/**
+	 * Returns the number assigned to the train.
+	 *
+	 * @return The train number.
+	 */
   public int getTrainNumber() {
 
 	return trainNumber;
   }
 
+	/**
+	 * Sets the train number after validating the input.
+	 *
+	 * @param trainNumber The number assigned to the train. It needs to be a positive number.
+	 */
   public void setTrainNumber(int trainNumber) {
 
 	if (CheckValid.checkValidInt
@@ -66,12 +108,21 @@ public class TrainDeparture {
 	}
   }
 
-
+	/**
+	 * Returns the destination of the train.
+	 *
+	 * @return The destination of the train.
+	 */
   public String getDestination() {
 
 	return destination;
   }
 
+	/**
+	 * Sets the destination of the train after validating the input.
+	 *
+	 * @param destination The destination of the train. It must be a valid string.
+	 */
   public void setDestination(String destination) {
 
 	if (CheckValid.checkValidString(destination, "Destination needs to be a string")) {
@@ -79,11 +130,21 @@ public class TrainDeparture {
 	}
   }
 
+	/**
+	 * Returns the track from which the train departs.
+	 *
+	 * @return The track number.
+	 */
   public int getTrack() {
 
 	return track;
   }
 
+	/**
+	 * Sets the track of the train after validating the input.
+	 *
+	 * @param track The track from which the train departs. It needs to be a positive number.
+	 */
   public void setTrack(int track) {
 
 	if (CheckValid.checkValidTrackNumber(track, "Track needs to be a positive number")) {
@@ -91,11 +152,21 @@ public class TrainDeparture {
 	}
   }
 
+	/**
+	 * Returns the delay of the train.
+	 *
+	 * @return The delay in the format hh:mm.
+	 */
   public String getDelay() {
 
 	return delay;
   }
 
+	/**
+	 * Sets the delay of the train after validating the input.
+	 *
+	 * @param delay The delay of the train. It must be a valid string in the format hh:mm.
+	 */
   public void setDelay(String delay) {
 
 	if (CheckValid.checkValidString(delay, "delay needs to be a valid string") &&
@@ -104,6 +175,11 @@ public class TrainDeparture {
 	}
   }
 
+	/**
+	 * Returns a string representation of the TrainDeparture instance, displaying all the details.
+	 *
+	 * @return A string representation of the train departure details.
+	 */
   public String display() {
 	StringBuilder sb = new StringBuilder();
 	sb.append("Train number: ").append(trainNumber).append("\t");
