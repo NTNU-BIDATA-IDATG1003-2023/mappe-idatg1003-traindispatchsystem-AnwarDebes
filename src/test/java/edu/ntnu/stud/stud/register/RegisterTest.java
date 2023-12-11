1c3ef55e7b6d1f2defc26c58c6f95cd100a1bf3c
anward@stud.ntnu.no <anwardebs6@gmail.com>
2023-12-11 11:41:38 +0100

    Adding positive and negative tests in the RegisterTest class

--- src/test/java/edu/ntnu/stud/stud/register/RegisterTest.java
+++ src/test/java/edu/ntnu/stud/stud/register/RegisterTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import edu.ntnu.stud.entity.TrainDeparture;
 import edu.ntnu.stud.register.Register;
+import java.time.format.DateTimeParseException;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,7 +18,7 @@ Register register;
   void setUp() {
      register = new Register();
      trainDeparture = new TrainDeparture(LocalTime.parse("12:00"), "L1", 1, "Oslo", 1, LocalTime.parse("00:00"));
-    TrainDeparture trainDeparture1 = new TrainDeparture(LocalTime.parse("15:00"), "L2", 2, "Oslo", 2, LocalTime.parse("10:00"));
+    TrainDeparture trainDeparture1 = new TrainDeparture(LocalTime.parse("15:00"), "L2", 2, "Oslo", 2, LocalTime.parse("00:10"));
     register.addTrainDeparture(trainDeparture);
     register.addTrainDeparture(trainDeparture1);
   }
@@ -34,7 +36,7 @@ Register register;
       fail("Should throw exception");
 
     } catch (IllegalArgumentException e) {
-      assertEquals("List is empty", e.getMessage());
+      assertEquals("Set is empty", e.getMessage());
     }
   }
 
@@ -69,37 +71,216 @@ Register register;
   }
 
   @Test
-  void getTrainDepartureWithDestination() {
-
+  void getTrainDepartureWithDestinationPositive() {
+    List<TrainDeparture> departures = register.getTrainDepartureWithDestination("Oslo");
+    assertFalse(departures.isEmpty());
+    assertEquals("Oslo", departures.get(0).getDestination());
+  }
+  @Test
+  void getTrainDepartureWithDestinationNegative() {
+    try {
+      register.getTrainDepartureWithDestination("Unknown");
+      fail("Should throw exception for unknown destination");
+    } catch (IllegalArgumentException e) {
+      assertEquals("Destination does not exist", e.getMessage());
+    }
+  }
+  @Test
+  void getTrainDepartureWithDepartureTimePositive() {
+    List<TrainDeparture> departures = register.getTrainDepartureWithDepartureTime("12:00");
+    assertFalse(departures.isEmpty());
+    assertEquals(LocalTime.parse("12:00"), departures.get(0).getDepartureTime());
+  }
+  @Test
+  void getTrainDepartureWithDepartureTimeNegative() {
+    try {
+      register.getTrainDepartureWithDepartureTime("25:00");
+      fail("Should throw exception for invalid time");
+    } catch (IllegalArgumentException | DateTimeParseException e) {
+      assertEquals("Text '25:00' could not be parsed: Invalid value for HourOfDay (valid values 0 - 23): 25", e.getMessage());
+    }
   }
 
   @Test
-  void getTrainDepartureWithDepartureTime() {
-
+  void removeTrainDeparturesPositive() {
+    assertEquals(2, register.getTrainDepartures().size());
+    register.removeTrainDepartureByTime("14:00");
+    // Expecting that after removal, only one train departure remains
+    assertEquals(1, register.getTrainDepartures().size());
   }
 
   @Test
-  void getTrainDepartureWithinTime() {
-
+  void testRemoveTrainDeparturesNegative() {
+    register.removeTrainDepartureByTime("19:00"); // This should remove all departures as they are before 19:00
+    try {
+      register.getTrainDepartures();
+      fail("Should throw exception because the set is now empty");
+    } catch (IllegalArgumentException e) {
+      assertEquals("Set is empty", e.getMessage());
+    }
   }
 
   @Test
-  void removeTrainDepartures() {
-
+  void removeTrainDeparturesByDestinationPositive() {
+    assertEquals(2, register.getTrainDepartures().size());
+    register.removeTrainDeparturesByDestination("Oslo", 0);
+    // Expecting that after removal, only one train departure remains
+    assertEquals(1, register.getTrainDepartures().size());
   }
-
   @Test
-  void testRemoveTrainDepartures() {
-
+  void removeTrainDeparturesByDestinationNegative() {
+    try {
+      register.removeTrainDeparturesByDestination("Unknown", 0);
+      fail("Should throw exception because the destination is unknown");
+    } catch (IllegalArgumentException e) {
+      assertEquals("Destination does not exist", e.getMessage());
+    }
   }
-
-  @Test
-  void removeTrainDeparturesByDestination() {
-
-  }
-
   @Test
   void sortTrainDepartures() {
+    List<TrainDeparture> sortedDepartures = register.sortTrainDepartures();
+    assertFalse(sortedDepartures.isEmpty());
+    assertTrue(sortedDepartures.get(0).getDepartureTime().isBefore(sortedDepartures.get(1).getDepartureTime()));
+  }
 
+
+
+
+
+  @Test
+  void addDuplicateTrainDepartureSameTimeAndTrack() {
+    assertThrows(IllegalArgumentException.class, () ->
+        register.addTrainDeparture(new TrainDeparture(LocalTime.parse("12:00"), "L1", 3, "Bergen", 1, LocalTime.parse("00:00"))));
+  }
+
+  @Test
+  void addDuplicateTrainDepartureSameTimeAndNumber() {
+    assertThrows(IllegalStateException.class, () ->
+        register.addTrainDeparture(new TrainDeparture(LocalTime.parse("12:00"), "L3", 1, "Bergen", 4, LocalTime.parse("00:00"))));
+  }
+
+  @Test
+  void getTrainDepartureWithInvalidNumber() {
+    assertThrows(IllegalArgumentException.class, () -> register.getTrainDepartureWithNumber(-1));
+  }
+
+  @Test
+  void getTrainDepartureWithInvalidDestination() {
+    assertThrows(IllegalArgumentException.class, () -> register.getTrainDepartureWithDestination("NonExistent"));
+  }
+
+  @Test
+  void getTrainDepartureWithInvalidTime() {
+    assertThrows(DateTimeParseException.class, () -> register.getTrainDepartureWithDepartureTime("24:00"));
+  }
+
+  @Test
+  void getTrainDepartureWithInvalidTrack() {
+    assertThrows(IllegalArgumentException.class, () -> register.getTrainDepartureWithTrack(-1));
+  }
+
+  @Test
+  void getTrainDepartureWithInvalidLane() {
+    assertThrows(IllegalArgumentException.class, () -> register.getTrainDepartureWithLane("NonExistentLane"));
+  }
+
+  @Test
+  void getTrainDepartureWithInvalidDelay() {
+    assertThrows(IllegalArgumentException.class, () -> register.getTrainDepartureWithDelay("99:99"));
+  }
+
+  @Test
+  void removeTrainDepartureByInvalidNumber() {
+    try
+    {
+        register.removeTrainDepartureByNumber(-1, 0);
+        fail("Should throw exception");
+        } catch (IllegalArgumentException e) {
+        assertEquals("Train number needs to be a positive number", e.getMessage());
+    }
+  }
+
+  @Test
+  void removeTrainDepartureByInvalidTrack() {
+    try
+    {
+        register.removeTrainDeparturesByTrack(-1, 0);
+        fail("Should throw exception");
+        } catch (IllegalArgumentException e) {
+        assertEquals("Track number does not exist", e.getMessage());
+    }
+  }
+
+  @Test
+  void removeTrainDepartureByInvalidLane() {
+    try
+    {
+        register.removeTrainDeparturesByLane("Lane does not exist", 0);
+        fail("Should throw exception");
+        } catch (IllegalArgumentException e) {
+        assertEquals("Lane does not exist", e.getMessage());
+    }
+  }
+
+  @Test
+  void removeTrainDepartureByInvalidDestination() {
+    try
+    {
+        register.removeTrainDeparturesByDestination("Destination does not exist", 0);
+        fail("Should throw exception");
+        } catch (IllegalArgumentException e) {
+        assertEquals("Destination does not exist", e.getMessage());
+    }
+  }
+
+  @Test
+  void sortEmptyTrainDepartures() {
+    register = new Register(); // Resetting the register
+    List<TrainDeparture> sortedDepartures = register.sortTrainDepartures();
+    assertTrue(sortedDepartures.isEmpty());
+  }
+
+
+
+  @Test
+  void filterDeparturesByInvalidTime() {
+    try{
+      register.filterDeparturesByTime("25:00");
+      fail("Should throw exception for invalid time");
+    } catch (IllegalArgumentException | DateTimeParseException e) {
+      assertEquals("Text '25:00' could not be parsed: Invalid value for HourOfDay (valid values 0 - 23): 25", e.getMessage());
+    }
+  }
+
+  @Test
+  void filterDeparturesByInvalidTrack() {
+    assertThrows(IllegalArgumentException.class, () -> register.filterDeparturesByTrack(-1));
+  }
+
+  @Test
+  void filterDeparturesByInvalidLine() {
+    assertThrows(IllegalArgumentException.class, () -> register.filterDeparturesByLine("NonExistentLine"));
+  }
+
+  @Test
+  void filterDeparturesByInvalidDestination() {
+    assertThrows(IllegalArgumentException.class, () -> register.filterDeparturesByDestination("NonExistentDestination"));
+  }
+
+  @Test
+  void filterDeparturesByInvalidDelay() {
+    assertThrows(IllegalArgumentException.class, () -> register.filterDeparturesByDelay("99:99"));
+  }
+
+  @Test
+  void getLaneMapEmpty() {
+    register = new Register(); // Resetting the register
+    assertThrows(IllegalArgumentException.class, register::getLaneMap);
+  }
+
+  @Test
+  void getLaneListEmpty() {
+    register = new Register(); // Resetting the register
+    assertThrows(NullPointerException.class, register::getLaneList);
   }
 }
